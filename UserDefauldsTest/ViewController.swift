412926beb596738138f8c1b3eb4e51c1dfdfb0d0
Alex <alex171274@rambler.ru>
2020-03-18 15:11:41 +0300

    Initial Commit

---
+++ UserDefauldsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefauldsTest
+//
+//  Created by Alex on 18.03.2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
